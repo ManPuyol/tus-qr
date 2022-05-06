4f582dc8451b9df8a5ac87517a04dcb8c555535a
Manuehh <manuel.ortega@gmail.com>
2022-05-06 08:56:58 +0200

    confirm if willPass

--- src/App.js
+++ src/App.js
@@ -4,17 +4,8 @@ import {QRCodeSVG} from 'qrcode.react';
 import usePromise from 'react-promise';
 import QRScan from './119444409-c6367d00-bd5d-11eb-8881-6cba01cc0d5e.png';
 
-const QR = () => {
-  const {value, loading} = usePromise(promise)
 
-  if (loading) return null
-  return (<QRCodeSVG
-            level="Q"
-            style={{ width: 256 }}
-          value={JSON.stringify(value.json)}
-          />)
-}
-
+const USERID = 483939
 //define new promise as http request
 const promise = new Promise((resolve) => {
   fetch('http://localhost:3000/generate', {
@@ -24,21 +15,23 @@ const promise = new Promise((resolve) => {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      "ID":483939
+      "ID":USERID
     })
   })
   .then(res => res.json())
   .then(json => {
-    console.log(json)
     resolve({json})
   })
 })
 
 function App() {
 
-  const [value, setValue] = React.useState('')
+  const [terminal, setTerminal] = React.useState('')
+  const [willPass, setWillPass] = React.useState(false)
   const [confirmation, setConfirmation] = React.useState(true)
+ 
   const sendConfirmation = async () => {
+    if(willPass){
 
     fetch('http://localhost:3000/confirm', {
       method: 'POST',
@@ -47,15 +40,37 @@ function App() {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        "ID":483939
+        "ID":USERID
       })
     })
     .then(res => res.json())
     .then(json => {
       console.log(json)
-      setValue(json.balance)
-      setConfirmation(false)
+    setTimeout(() => setTerminal(json.balance), 0);
+    setTimeout(() => setConfirmation(false), 0);
     })
+  }else{
+    setTimeout(() => setTerminal('Saldo insuficiente'), 0);
+    setTimeout(() => setConfirmation(true), 0);
+    }
+  }
+
+  const QR = () => {
+    const {value, loading} = usePromise(promise)
+  
+    if (loading) {return null}
+    else{
+    
+    
+    setTimeout(() => setWillPass(value.json.willPass), 0);
+    
+    console.log(value.json)
+
+    return (<QRCodeSVG
+              level="Q"
+              style={{ width: 256 }}
+            value={JSON.stringify(value.json)}
+            />)}
   }
 
   return (
@@ -66,7 +81,9 @@ function App() {
       </div>
       <div onClick={() => {sendConfirmation()}} style={styles.qrcode}>
         <img src={ QRScan } alt='scan' style={styles.qrScan}/>
-        {confirmation ? <h3></h3> : <h3>Balance: {JSON.stringify(value)}</h3>}
+        {confirmation ? 
+         <h3 style={styles.red}>{!willPass && terminal}</h3> :
+         <h3 style={styles.green}>Balance: {JSON.stringify(terminal)}</h3>}
       </div>
     </div>
   );
@@ -87,7 +104,14 @@ const styles = {
   qrScan: {
     margin: 'auto',
     display: 'block',
-    width: '20%',
-    height: '20%',
+    width: '250px',
+    height: '200px',
   },
+  green: {
+    color: 'green',
+  },
+  red: {
+    color: 'red',
+  },
+    
 };
